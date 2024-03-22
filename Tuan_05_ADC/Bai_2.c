972d8fb5e8d03e3ee8a4d39842134b51797b3ce9
kimdamitung <150353472+kimdamitung@users.noreply.github.com>
2024-03-22 16:30:57 +0700

    Completed Bai_2 ADC
    nothing

---
+++ Tuan_05_ADC/Bai_2.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <errno.h>
+#include <linux/fs.h>
+#include <string.h>
+#include <termios.h>
+
+#define ON 1
+#define OFF 0
+#define PWM_IOCTL_SET_FREQ 1
+#define PWM_IOCTL_STOP 0
+#define ESC_KEY 0x1b
+
+void set_buzzer_freq(int fd, int freq){
+	int ret = ioctl(fd, PWM_IOCTL_SET_FREQ, freq);
+	if(ret < 0){
+		perror("Set the frequency of the buzzer\n");
+		exit(1);
+	}
+}
+
+void stop_buzzer_freq(int fd){
+	int ret = ioctl(fd, PWM_IOCTL_STOP);
+	if(ret < 0){
+		perror("Stop the buzzer\n");
+		exit(1);
+	}
+	close(fd);
+}
+
+int statePWM(int value){
+	if(value >= 0 && value < 400){
+		return 400;
+	}else if(value >= 400 && value < 800){
+		return 800;
+	}else if(value >= 800 && value < 1023){
+		return 1200;
+	}
+}
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	printf("Press Ctrl-C to stop\n"); 
+ 	int adc_fd = open("/dev/adc", 0);
+ 	int fd = open("/dev/leds", 0); 
+	int pwm_fd = open("/dev/pwm", 0);
+ 	if (adc_fd < 0) { 
+ 		perror("open ADC device:"); 
+ 		return 1; 
+ 	}
+ 	if (fd < 0) { 
+ 		perror("open LED device:"); 
+ 		return 1; 
+ 	}
+ 	while(1) { 
+ 		char buffer[30]; 
+ 		int len = read(adc_fd, buffer, sizeof(buffer)-1); 
+ 		if (len > 0){ 
+ 			buffer[len] = '\0'; 
+ 			int value = -1; 
+ 			sscanf(buffer, "%d", &value);//doc du lieu theo dinh dang 
+ 			printf("ADC Value: %d\n", value);
+ 			int stateValue = statePWM(value);
+			set_buzzer_freq(pwm_fd, stateValue);
+ 		}else { 
+ 			perror("read ADC device:"); 
+ 			return 1; 
+ 		}
+ 		usleep(500* 1000); 
+ 	} 
+ 	close(fd); 
+	return 0;
+}
